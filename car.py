cfe9a21cc742edfd756514295aa4f029eddbf706
Elizabeth <elizabeth@cs4africa.com>
2017-02-07 18:57:53 +0300

    changed files after getting some advice from my facilitator.

--- car.py
+++ car.py
@@ -1,60 +1,33 @@
 class Car(object):
-  
-  def __init__(self,name='General',model='GM',car_type='honda'):
-    
+  def __init__(self,name='General',model='GM',car_type='honda'): 
     self.car_type = car_type
     self.model = model
     self.name = name
-    self.speed = 0
-    
-    if name=='Porshe' or name=='Koenigsegg':
-      
-      self.num_of_doors = 2
-      
-    else:
-      
-      self.num_of_doors = 4
-      
-    if car_type=='trailer':
-      
-      self.num_of_wheels = 8
-      
-    else:
-      
-      self.num_of_wheels = 4
-      
+    self.speed = 0 
+    if name=='Porshe' or name=='Koenigsegg': 
+      self.num_of_doors = 2  
+    else: 
+      self.num_of_doors = 4 
+    if car_type=='trailer': 
+      self.num_of_wheels = 8 
+    else: 
+      self.num_of_wheels = 4 
   def doors(self, num_of_doors):
-    
     pass
-
-  def drive(self, moving_man):
-    
+  def drive(self, moving_man):  
     return moving_man
-
-  def drive(self, spid):
-    
-    if self.car_type=='trailer':
-      
-      self.speed = spid*11
-      
+  def drive(self, spid):  
+    if self.car_type=='trailer':   
+      self.speed = spid*11   
+    else:  
+      self.speed = 10**spid  
+    return self 
+  def wheels(self, num_of_wheels): 
+    return num_of_wheels  
+  def is_saloon(self): 
+    if self.car_type=='trailer':  
+      return False  
     else:
-      
-      self.speed = 10**spid
-      
-    return self
-    
-  def wheels(self, num_of_wheels):
-    
-    return num_of_wheels
-    
-  def is_saloon(self):
-    
-    if self.car_type=='trailer':
-      
-      return False
-      
-    else:
-      
       return True
         
   
--- data_types.py
+++ data_types.py
@@ -1,39 +1,21 @@
 def data_type(input):
-  
-  if type(input)==str:
-    
-    return len(input)
-    
-  elif type(input)==bool:
-    
-    return input
-    
-  elif type(input)==int:
-    
-    if input<100:
-      
-      return 'less than 100'
-      
-    elif input>100:
-      
-      return 'more than 100'
-      
+  if type(input)==str: 
+    return len(input) 
+  elif type(input)==bool: 
+    return input  
+  elif type(input)==int:  
+    if input<100:  
+      return 'less than 100'  
+    elif input>100:  
+      return 'more than 100' 
     else:
-      
-      return 'equal to 100'
-      
+      return 'equal to 100'  
   elif type(input)==list:
-    
-    if len(input)<3:
-      
-      return None
-      
-    else:
-      
-      return input[2]
-      
-  else:
-    
+    if len(input)<3: 
+      return None  
+    else:  
+      return input[2]    
+  else: 
     return 'no value'
 
         
